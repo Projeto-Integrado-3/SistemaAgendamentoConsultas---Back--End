a057306ab76361c9a378942e9d81e0404f76ba1e
Rayane Amaro <rayaneamaro96@gmail.com>
2024-09-03 00:04:29 -0300

    Create PlanoSaude.java

---
+++ src/br/com/clinica/model1/PlanoSaude.java
@@ -0,0 +1,16 @@
+package br.com.clinica.model1;
+
+public class PlanoSaude {
+	    int idPlanoSaude;
+	    String nomePlano;
+	    String cobertura;
+
+	    public PlanoSaude(int idPlanoSaude, String nomePlano, String cobertura) {
+	        this.idPlanoSaude = idPlanoSaude;
+	        this.nomePlano = nomePlano;
+	        this.cobertura = cobertura;
+	    }
+
+
+	
+}
