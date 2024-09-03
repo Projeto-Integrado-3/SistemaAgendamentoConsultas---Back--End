7fbcb499e41306d6b1f4a3dfc9c2732195f1a488
Valdeilson Lima <valdeilsonbdl56@gmail.com>
2024-09-02 21:07:12 -0300

    Iniciando o projeto

---
+++ src/br/com/clinica/model1/Paciente.java
@@ -0,0 +1,31 @@
+package br.com.clinica.model1;
+
+public class Paciente {
+	 int idPaciente;
+     String nome;
+     String cpf;
+     String dataNascimento;
+     String sexo;
+     String endereco;
+     int numero;
+     String bairro;
+     String cidade;
+     String uf;
+     String telefone;
+     String email;
+     
+     public Paciente(int idPaciente, String nome, String cpf, String dataNascimento, String sexo, String endereco, int numero, String bairro, String cidade, String uf, String telefone, String email) {
+         this.idPaciente = idPaciente;
+         this.nome = nome;
+         this.cpf = cpf;
+         this.dataNascimento = dataNascimento;
+         this.sexo = sexo;
+         this.endereco = endereco;
+         this.numero = numero;
+         this.bairro = bairro;
+         this.cidade = cidade;
+         this.uf = uf;
+         this.telefone = telefone;
+         this.email = email;
+     }
+}
