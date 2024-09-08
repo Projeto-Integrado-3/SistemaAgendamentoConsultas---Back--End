77f75dbdfefdf9029b3df4bd81ccb1b27a592d98
Rayane Amaro <rayaneamaro96@gmail.com>
2024-09-08 01:17:36 -0300

    Add files via upload

---
+++ src/br/com/clinica/model1/Consulta.java
@@ -0,0 +1,27 @@
+
+package br.com.clinica.model1;
+import java.util.Date;
+
+public class Consulta {
+    int idConsulta;
+    PlanoSaude planoSaude;
+    ProfissionalSaude profissionalSaude;
+    Paciente paciente;
+    double precoConsulta;
+    Date horaConsulta;
+    Date dataConsulta;
+    String statusConsulta;
+
+    public Consulta(int idConsulta, PlanoSaude planoSaude, ProfissionalSaude profissionalSaude, Paciente paciente, double precoConsulta, Date horaConsulta, Date dataConsulta, String statusConsulta) {
+        this.idConsulta = idConsulta;
+        this.planoSaude = planoSaude;
+        this.profissionalSaude = profissionalSaude;
+        this.paciente = paciente;
+        this.precoConsulta = precoConsulta;
+        this.horaConsulta = horaConsulta;
+        this.dataConsulta = dataConsulta;
+        this.statusConsulta = statusConsulta;
+    }
+
+    
+}
